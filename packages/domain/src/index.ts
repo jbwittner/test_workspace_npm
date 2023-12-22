fd0be9899ef69bb524438c16256916e8443e0715
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-22 23:49:31 +0100

    Friday 5pm

---
+++ packages/domain/src/index.ts
@@ -0,0 +1,11 @@
+interface TotoDomain {
+    one: string,
+    two: string
+}
+
+const blabla: TotoDomain = {
+    one: "a",
+    two: "b"
+}
+
+console.log(blabla)
---
+++ packages/infra/src/index.ts
@@ -0,0 +1,11 @@
+interface TotoInfra {
+    one: string,
+    two: string
+}
+
+const bla: TotoInfra = {
+    one: "a",
+    two: "b"
+}
+
+console.log(bla)
