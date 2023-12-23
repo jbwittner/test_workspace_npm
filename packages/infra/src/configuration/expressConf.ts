0b0002bb6856f983e4c8a1acd36b5fa04363c422
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-23 23:22:22 +0100

    Chuck Norris Emailed Me This Patch... I'm Not Going To Question It

--- packages/infra/src/configuration/expressConf.ts
+++ packages/infra/src/configuration/expressConf.ts
@@ -2,10 +2,6 @@ import express from 'express'
 
 export const app = express();
 
-export const initControllerAndInject = (apiProductService: ApiProductService, logger: AppLogger) => {
-    apiProductController(apiProductService, logger);
-}
-
 export const startExpressServer = async (port: number) => {
     await new Promise((resolve) => app.listen(port, () => resolve(app)));
 }
