38978c60dad7f8fdb7c51b1458421c58c9deeb40
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-24 15:23:46 +0100

    What happens in vegas stays in vegas

---
+++ packages/domain/test/userdomainservice/CreateUser.test.ts
@@ -0,0 +1,25 @@
+import { UserDomainService, UserSpi } from "../../src";
+
+describe('Test createUser method', () => {
+    let userDomainService: UserDomainService
+    let userSpi: jest.Mocked<UserSpi>  = {
+        save: jest.fn(),
+        findUser: jest.fn()
+    }
+
+    beforeEach(() => {
+        userSpi.save.mockClear()
+        userSpi.findUser.mockClear()
+        userDomainService = new UserDomainService(userSpi)
+    });
+
+    test('Create user Ok', () => {
+        userSpi.save.mockImplementation((user) => user)
+        const userName = "userToto"
+        const user = userDomainService.createUser(userName)
+        expect(user.getUserName()).toBe(userName);
+    });
+  });
+  
+  
+
