9211a9ba38d0ae30879c2972830917795a85782d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-27 16:05:56 +0100

    Programming the flux capacitor

--- domain/src/__tests__/service/userdomainservice/GetUser.test.ts
+++ domain/src/__tests__/service/userdomainservice/GetUser.test.ts
@@ -1,8 +1,8 @@
 import { v4 as uuidv4 } from 'uuid'
-import { UserDomainService } from '../../../service'
 import { UserSpi } from '../../../spi'
 import { DomainFaker } from '../../testtools/DomainFaker'
 import { faker } from '@faker-js/faker'
+import { UserDomainService } from '../../../service'
 
 describe('Test getUser method', () => {
   let userDomainService: UserDomainService
