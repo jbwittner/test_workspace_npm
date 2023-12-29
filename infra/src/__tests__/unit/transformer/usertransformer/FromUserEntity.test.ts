c7ee32b6da26be79818c1bcef49a0a945823f805
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-29 17:35:19 +0100

    Revert "fuckup".

--- infra/src/__tests__/unit/transformer/usertransformer/FromUserEntity.test.ts
+++ infra/src/__tests__/unit/transformer/usertransformer/FromUserEntity.test.ts
@@ -1,25 +1,20 @@
 import { faker } from '@faker-js/faker'
 import { UserTransformer } from '../../../../transformer/UserTransformer'
-import { UserEntity, initUserEntityModel } from '../../../../spi/models/UserEntity'
-import { v4 as uuidv4 } from 'uuid'
 import { Sequelize } from 'sequelize'
+import { ModelManager } from '../../../../spi/models/ModelManager'
+import { UserEntity } from '../../../../spi/models/UserEntity'
+import { v4 as uuidv4 } from 'uuid'
 
 describe('Test toUserEntity method', () => {
+  ModelManager(new Sequelize({ dialect: 'mysql' }))
   const userTransformer = UserTransformer()
 
-  beforeAll(() => {
-    const sequelize = new Sequelize()
-
-    const mockUserSpi: jest.Mocked<Sequelize>}
-
-    initUserEntityModel(sequelize)
-  })
-
   test('Ok', () => {
-    const userEntity = new UserEntity()
-    userEntity.userName = faker.internet.userName()
-    userEntity.userId = uuidv4()
-
+    ModelManager(new Sequelize({ dialect: 'mysql' }))
+    const userEntity = new UserEntity({
+      userName: faker.internet.userName(),
+      userId: uuidv4()
+    })
     const user = userTransformer.fromUserEntity(userEntity)
     expect(user.getUserId()).toBe(userEntity.userId)
     expect(user.getUserName()).toBe(userEntity.userName)
