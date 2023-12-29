6b862282311f920c5928b1ebe9d542375461c312
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-29 23:38:15 +0100

    I just evaluated random code in my console

--- infra/src/spi/models/UserGroupEntity.ts
+++ infra/src/spi/models/UserGroupEntity.ts
@@ -9,15 +9,15 @@ export class UserGroupEntity {
   userGroupId: string
 
   @ManyToOne(() => UserEntity) // note: we will create author property in the Photo class below
-  userEntity: Promise<UserEntity>
+  userEntity: UserEntity
 
   @ManyToOne(() => GroupEntity)
-  groupEntity: Promise<GroupEntity>
+  groupEntity: GroupEntity
 
   constructor(userGroupId: string, userEntity: UserEntity, groupEntity: GroupEntity){
     this.userGroupId = userGroupId
-    this.userEntity = Promise.resolve(userEntity)
-    this.groupEntity = Promise.resolve(groupEntity)
+    this.userEntity = userEntity
+    this.groupEntity = groupEntity
   }
 
 }
